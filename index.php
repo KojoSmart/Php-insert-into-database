dabdb70db0f7af0248a4969aa0559410a5ef678f
KojoSmart <112355960+KojoSmart@users.noreply.github.com>
2022-10-28 11:35:18 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+  
+
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<title> </title>
+</head>
+<body>
+
+
+<form action="sign up.php" method="POST">
+  <input type="text"  name="first" placeholder = "FirstName">
+  <br>
+  <input type="text"  name="last" placeholder = "LastName">
+  <br>
+  <input type="text"  name="email" placeholder = "E-mail">
+  <br>
+  <input type="text"  name="uid" placeholder = "Username">
+  <br>
+  <input type="text"  name="pwd" placeholder = "password">
+   
+  <br>
+  <button type="submit" name="submit"> Sign Up</button>
+  
+
+</form>
+<?php
+
+
+
+
+?>
+</body>
+</html>
